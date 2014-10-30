758477ec2ffddddb3b81516d76815afc5310d5db
Stefan C. Mueller <scm@smurn.org>
2014-10-29 22:33:54 -0700

    Added licence and unit-tests. Fixed some bugs.

--- cish/pyenv.py
+++ cish/pyenv.py
@@ -1,3 +1,32 @@
+# Copyright (c) 2014, Stefan C. Mueller
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import os.path
 import sys
 
@@ -14,9 +43,10 @@ class PyEnv(object):
         You might want to consider using the factory methods provided in this module.
         """
         self.search_paths = search_paths
-        self.extensions = ['', 'w.exe' ,'.exe']
+        self.exec_patterns = ['{name}' ,'{name}.exe']
+        self.python_patterns = ['{name}', 'w{name}.exe' ,'{name}.exe']
 
-    def find_executable(name):
+    def find_executable(self, name):
         """
         Finds an executable with the given name in this enviroment.
         
@@ -24,19 +54,20 @@ class PyEnv(object):
 
         :raises ValueError: if the exectuable could not be found.
         """
-        candidate_names = [name + ext for ext in self.extensions]
+        
+        patterns = self.python_patterns if name == "python" else self.exec_patterns
+        candidate_names = [ext.format(name=name) for ext in patterns]
         candidates = [os.path.join(path, name) for path in self.search_paths for name in candidate_names]
    
-        match = None
         for candidate in candidates:
             if os.path.exists(candidate):
-                match = candidate
-                break
-        else:
-            raise ValueError("Unable to find {name}. Looked at {candidates}".format(
-                name=repr(name),
-                candidates=", ".join(candidates)
-            ))
+                return candidate
+            
+        raise ValueError("Unable to find {name}. Looked at {candidates}".format(
+            name=repr(name),
+            candidates=", ".join(candidates)
+        ))
+        
 
 
 def interpeter_pyenv():
@@ -57,11 +88,11 @@ def pyenv_from_interpreter(exe):
  
     :returns: Instance of :class:`PyEnv`
     """
-    exeabs = os.path.abspath(interpreter)
+    exeabs = os.path.abspath(exe)
     if not os.path.exists(exeabs):
         raise ValueError("Python interpreter {exe} does not exist here {exeabs}.".format(exe=exe, exeabs=exeabs))
     
-    path = os.path.dirname(interpreter)
+    path = os.path.dirname(exeabs)
     return _pyenv_from_paths(path, ["Scripts", "scripts"])
 
 
@@ -89,7 +120,7 @@ def _pyenv_from_paths(path, subdirs):
     paths = [path] + [os.path.join(path, subdir) for subdir in subdirs]
     search_paths = []
     for path in paths:
-        if os.isdir(path): 
+        if os.path.isdir(path): 
             search_paths.append(path)
     if not search_paths:
         raise ValueError("Python environment not found. None of the directories exists {dirs}".format(
---
+++ cish/test_pyenv.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2014, Stefan C. Mueller
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+import os.path
+import shutil
+import tempfile
+
+from cish import pyenv
+
+class TestPyEnv(unittest.TestCase):
+    """
+    Unit-tests for :class:`pyenv.PyEnv`.
+    """
+
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+    
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    def test_linux_style(self):
+        """
+        Python installed using linux distribution packages
+        usually ends up in '/usr/bin', as do all the utilities.
+        """
+        self.create_files(["bin/python",
+                           "bin/pip",
+                           "bin/virtualenv"])
+        env = pyenv.pyenv_from_interpreter(self.get_path("bin/python"))
+        self.assertEqual(env.find_executable("pip"), self.get_path("bin/pip"))
+        
+    def test_windows_style(self):
+        """
+        Windows installations have the utilities in a separate directory.
+        """
+        self.create_files(["python.exe",
+                           "Scripts/pip",
+                           "Scripts/virtualenv"])
+        env = pyenv.pyenv_from_interpreter(self.get_path("python.exe"))
+        self.assertEqual(env.find_executable("pip"), self.get_path("Scripts/pip"))
+        
+    def test_prefer_wpython(self):
+        """
+        Windows installations have the utilities in a separate directory.
+        """
+        self.create_files(["python.exe",
+                           "wpython.exe"])
+        env = pyenv.pyenv_from_interpreter(self.get_path("python.exe"))
+        self.assertEqual(env.find_executable("python"), self.get_path("wpython.exe"))
+        
+    def test_w_prefix_only_for_python(self):
+        """
+        On windows installations the interpreter might have a `w` prefix,
+        but this is not used for other executables, so we should not 
+        return the wrong one.
+        """
+        self.create_files(["python.exe",
+                           "wpip.exe",
+                           "pip.exe"])
+        env = pyenv.pyenv_from_interpreter(self.get_path("python.exe"))
+        self.assertEqual(env.find_executable("pip"), self.get_path("pip.exe"))
+        
+    def get_path(self, path):
+        """
+        Returns the absolute path to a file relative to the temporary directory.
+        """
+        return os.path.join(self.tmpdir, path.replace("/", os.sep))
+
+    def create_files(self, files):
+        """
+        Takes a list of files (optionally with relative paths)
+        and creates them in the temporary directory.
+        """
+        files = [f.replace("/", os.sep) for f in files]
+        
+        for relative_file in files:
+            relative_file = relative_file.replace("/", os.pathsep)
+            relative_path, filename = os.path.split(relative_file)
+            absolute_path = os.path.join(self.tmpdir, relative_path)
+            if not os.path.exists(absolute_path):
+                os.makedirs(absolute_path)
+            absolute_file = os.path.join(absolute_path, filename)
+            with open(absolute_file, 'w') as f:
+                f.write(relative_file)
