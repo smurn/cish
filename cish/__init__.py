910e01d5e6d701e7554449b365e392a590be08de
Stefan C. Mueller <scm@smurn.org>
2014-10-30 23:18:00 +0000

    Exposed PyEnv factory functions to the public API.

--- cish/__init__.py
+++ cish/__init__.py
@@ -26,3 +26,10 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+
+
+from cish.pyenv import from_config
+from cish.pyenv import from_interpreter
+from cish.pyenv import from_virtualenv
+
+
